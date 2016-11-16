c43adbe294307642b0d996b2e73ddbe9eb9b007a
yinyong02 <yinyong02@baidu.com>
2016-11-16 09:52:08 +0800

    validate duplicated

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,15 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+
+## [1.1.0] - 2016-11-16
+### Changed
+- validate duplicated
+- validate duplicated version keys
+
+## 1.0.0 - 2016-11-14
 ### Added
 - Initialize and publish this project with test cases and RN 0.35.0
 
-[Unreleased]: https://github.com/super-fe/superfe-compatible-rn-versions/compare/1.0.0...HEAD
+[1.1.0]: https://github.com/super-fe/superfe-compatible-rn-versions/compare/1.0.0...1.1.0
+[Unreleased]: https://github.com/super-fe/superfe-compatible-rn-versions/compare/1.1.0...HEAD
--- test/test.js
+++ test/test.js
@@ -4,23 +4,38 @@ var semver = require('semver');
 var assert = require('assert');
 
 describe('versions', function () {
-  it('should be an array', function () {
-    assert.ok(Array.isArray(versions));
-  });
-  it('each should be a legal version', function (done) {
-    this.timeout(5e3);
-
-    pkgVersions('react-native', function (err, vers) {
-      if (err) {
-        return done(err);
-      }
-      versions.forEach(function (version) {
-        assert.ok(semver.gte(version, '0.35.0'), version + ' >= 0.35.0 ');
-        assert.ok(vers.some(function (ver) {
-          return ver === version;
-        }), version + ' should be on npm');
-      });
-      done();
+    it('should be an array', function () {
+        assert.ok(Array.isArray(versions));
     });
-  });
+
+    it('each should be a valid version', function () {
+        versions.forEach(function (ver) {
+            assert.ok(semver.valid(ver));
+        });
+    });
+
+    it('never values are duplicated', function () {
+        var map = {};
+        versions.forEach(function (ver) {
+            assert.ok(!(ver in map));
+            map[ver] = 1;
+        });
+    });
+
+    it('each should be a legal npm version', function (done) {
+        this.timeout(5e3);
+
+        pkgVersions('react-native', function (err, vers) {
+            if (err) {
+                return done(err);
+            }
+            versions.forEach(function (version) {
+                assert.ok(semver.gte(version, '0.35.0'), version + ' >= 0.35.0 ');
+                assert.ok(vers.some(function (ver) {
+                    return ver === version;
+                }), version + ' should be on npm');
+            });
+            done();
+        });
+    });
 });
